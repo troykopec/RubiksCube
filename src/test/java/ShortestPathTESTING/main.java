ec7bda696b64de43629d36b88c74f4a22a1a2691
Troy Kopec <troykopec@gmail.com>
2023-12-12 20:11:12 -0500

    Complete with Header
    Solving Rubiks Cube Using Graph Theory is found under 'ShortestPathTESTING' folder.
    bin >> test >> ShortestPathTESTING

--- src/test/java/ShortestPathTESTING/main.java
+++ src/test/java/ShortestPathTESTING/main.java
@@ -1,5 +1,17 @@
 package ShortestPathTESTING;
-
+//////////////////////////////////////////////////////////
+//
+//  This code implements a way of solving a rubiks cube
+//  by using graph theory. Two trees are expanded, one
+//  from a solved cube, and one from a random cube,
+//  the front tree is checked for a solution using BFS
+//  during creation. If a solution is not found, DFS is
+//  used to traverse the back tree with multithreading,
+//  if a solution is found, its path will be traced and
+//  printed.
+//  - By Troy and Kevin
+//
+//////////////////////////////////////////////////////////
 public class main {
     public static void main(String[] args) {
         // Specify limits
